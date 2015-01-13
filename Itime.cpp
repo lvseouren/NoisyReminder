6226590ff7454875cb3b03042a47e4c9c6a09913
lvseouren <lvseouren@163.com>
2015-01-13 21:02:55 +0800

    上传项目
    程序差不多就这样了
    TODO：屏幕抖动的时候，用户能够看到提醒者那些贱贱的话——即，将提醒者窗口置于最前面

---
+++ Itime.cpp
@@ -0,0 +1,162 @@
+#include"Itime.h"
+
+Itime::Itime(int y,int m,int d,int hour,int min,int sec):year(y),month(m),day(d),hour(hour),minute(min),second(sec)
+{
+}
+Itime::Itime()
+{}
+Itime Itime::operator-(const Itime& rhs)
+{
+	int year_differ = year-rhs.year;
+	int mon_differ = month-rhs.month;
+	int day_differ = (day-rhs.day);
+	int hour_differ = (hour-rhs.hour);
+	int min_differ = (minute-rhs.minute);
+	int sec_differ = second-rhs.second;
+	return Itime(year_differ,mon_differ,day_differ,hour_differ,min_differ,sec_differ);
+}
+bool Itime::operator>(const Itime&rhs)
+{
+	Itime differ = *this-rhs;
+	if(differ.hour>0)
+	{
+		return true;
+	}
+	else if(differ.hour == 0)
+	{
+		if(differ.minute > 0)
+		{
+			return true;
+		}
+		else if(differ.minute == 0)
+		{
+			return differ.second>0?true:false;
+		}
+		else//hour = 0,minute<0
+		{
+			return false;
+		}
+	}
+	//hour<0
+	return false;
+
+
+		
+
+}
+int Itime::ItimeToSecond()
+{
+	bool yun_year = false;
+	if(year%400==0)
+		yun_year = true;
+	else if(year%4==0 && year%100!=0)
+		yun_year = true;
+	//todo:年和月的秒数转换得分情况讨论，暂不考虑
+	//int year_sec = yun_year?31622400*year:31536000*year;
+	//int mon_sec = 
+	//int day_sec = day*86400;
+	int hour_sec = hour*3600;
+	int min_sec = minute*60;
+	int result = hour_sec+min_sec+second;
+	return result;
+}
+string Itime::ItimeToString()
+{
+	string result;
+
+	char TimeSecStr[128];
+	char TimeMinStr[128];
+	char TimeHourStr[128];
+	sprintf_s(TimeSecStr,"%d",second);
+	sprintf_s(TimeMinStr,"%d",minute);
+	sprintf_s(TimeHourStr,"%d",hour);
+
+	result = TimeHourStr+string(":")+TimeMinStr+string(":")+TimeSecStr;
+	return result;
+}
+
+string Itime::ItimeLenToString()
+{
+	//修正时间
+	if(hour<0)
+	{
+		hour+=24;
+	}
+	if(minute<0)
+	{
+		--hour;
+		minute+=60;
+	}
+	if(second<0)
+	{
+		--minute;
+		second+=60;
+	}
+
+	string result;
+
+	char TimeSecStr[128];
+	char TimeMinStr[128];
+	char TimeHourStr[128];
+	sprintf_s(TimeSecStr,"%d",second);
+	sprintf_s(TimeMinStr,"%d",minute);
+	sprintf_s(TimeHourStr,"%d",hour);
+
+	result =string("共计") + TimeHourStr+string("h")+TimeMinStr+string("m")+TimeSecStr+string("s");
+	return result;
+}
+
+string Itime::ItimeToFileString()
+{
+	string result;
+
+	char TimeYearStr[128];
+	char TimeMonthStr[128];
+	char TimeDayStr[128];
+	sprintf_s(TimeYearStr,"%d",year);
+	sprintf_s(TimeMonthStr,"%d",month);
+	sprintf_s(TimeDayStr,"%d",day);
+
+	result = TimeYearStr+string("年")+TimeMonthStr+string("月")+TimeDayStr+string("日.txt");
+	return result;
+}
+
+
+ostream& operator<<(ostream& os,Itime time)
+{
+	os<<time.hour<<":"<<time.minute<<":"<<time.second;
+	return os;
+}
+
+Itime StrConvertToItime(const string&timeStr)
+{
+	int startHour;
+	int endHour;
+	int startMin;
+	int endMin;
+	int startSec;
+	int endSec;
+
+	//以下算法的前提是,':'只占用一个字符的长度
+	string test = ":";
+	int lengh = test.length();
+
+	startHour = 0;
+	endHour = timeStr.find(':');
+
+	startMin = endHour+1;
+	endMin = timeStr.rfind(':')-1;
+
+	startSec = endMin+2;
+	endSec = timeStr.length()-1;
+
+	string hourStr = timeStr.substr(startHour,endHour);
+	string minStr = timeStr.substr(startMin,endMin);
+	string secStr = timeStr.substr(startSec,endSec);
+
+	int hour = atoi(hourStr.c_str());
+	int min = atoi(minStr.c_str());
+	int sec = atoi(secStr.c_str());
+
+	return Itime(0,0,0,hour,min,sec);
+}
---
+++ main.cpp
@@ -0,0 +1,141 @@
+#include<iostream>
+#include<Windows.h>
+#include<Winuser.h>
+#include<shellapi.h>
+#include<time.h>
+
+#include"Itime.h"
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+Itime GetCurrentITime()//放到time.h中去
+{
+	time_t now;
+	time(&now);
+	struct tm* t = new tm();
+	localtime_s(t,&now);
+	return Itime(t->tm_year+1900,t->tm_mon+1,t->tm_mday,t->tm_hour,t->tm_min,t->tm_sec);
+}
+void StartShake();
+
+void main(void)
+{
+	//TODO:将点餐提醒时间改为由用户设置
+	cout<<"请输入提醒时间（格式为 hour：minute：second——格式输错我可不负责！）:"<<endl;
+	string remindTimeStr;
+	cin>>remindTimeStr;
+
+	//Itime BeginRemindTime(0,0,0,20,35,0);
+	Itime BeginRemindTime = StrConvertToItime(remindTimeStr);
+
+	cout<<"你设定的点餐时间是:"<<BeginRemindTime<<endl;
+
+	Itime timeBeforeSleep;
+	Itime timeWakeup;
+	int secToSleep=0;
+	bool sleeped = false;
+	string remindEatStr = "点餐";
+	string remindGoHomeStr = "回家";
+	
+	cout<<"别急，我在看着表呢！到时间了我会提醒你"<<remindEatStr<<"的."<<endl;
+	while(1)
+	{
+		timeWakeup = GetCurrentITime();
+		if(sleeped&&secToSleep!=0)
+		{
+			cout<<"我睡醒了，现在时间是："<<timeWakeup<<endl;
+		}
+		Itime currentTime = GetCurrentITime();
+		if(currentTime > BeginRemindTime)
+		{
+			cout<<"点餐啦，点餐啦，听不见？我摇，我摇摇摇！"<<endl;
+			StartShake();
+			cout<<"注意到了我了吧！逗比，点餐啦，不然一会饿死你！"<<endl;
+			Sleep(5000);
+			cout<<"什么，居然还没注意到我！我再摇摇摇！"<<endl;
+			StartShake();
+			ShellExecute(NULL,L"open",L"http://www.meican.com/corps/idreamsky/",NULL,NULL,SW_SHOWMAXIMIZED);
+			break;
+		}
+		else//算出离点餐时间还差多久，让程序先sleep一段时间。
+		{
+			secToSleep = (BeginRemindTime -currentTime).ItimeToSecond();
+			secToSleep -= 2;//提前两秒醒来
+			if(secToSleep<0)
+			{
+				secToSleep = 0;//防止时间小于0
+			}
+			timeBeforeSleep = currentTime;
+			if(secToSleep!=0)
+			{
+				cout<<"现在时间是："<<timeBeforeSleep<<endl;
+				cout<<"离"<<remindEatStr<<"时间还有点远，我先睡"<<secToSleep<<"秒"<<endl;
+			}
+			Sleep(secToSleep*1000);
+			sleeped = true;
+		}
+	}
+	cout<<"任务完成，记得"<<remindEatStr<<"，我滚了。"<<endl;
+	
+	//TODO：等待2s，关闭
+	Sleep(2000);
+}
+void StartShake()
+{
+    srand((unsigned)time(NULL));
+    int sysX,sysY;
+    sysX=GetSystemMetrics(SM_CXSCREEN);//获得屏幕宽度
+    sysY=GetSystemMetrics(SM_CYSCREEN);//获得屏幕高度
+	Itime t1= GetCurrentITime();
+    while(1)
+    {
+        HWND hWnd=GetDesktopWindow();//获得桌面句柄
+        for(hWnd=GetWindow(hWnd,GW_CHILD);hWnd;hWnd=GetWindow(hWnd,GW_HWNDNEXT))
+        {           
+            RECT rect;
+             
+            long style=GetWindowLong(hWnd,GWL_STYLE);//获得窗口类型
+            if(style&WS_VISIBLE)//窗口可视
+            {           
+                GetWindowRect(hWnd,&rect);//获得窗口矩形坐标
+                int posX,posY,cX,cY;
+                bool flag=!(rand()%2);
+                 
+                cX=rect.right-rect.left;
+                cY=rect.bottom-rect.top;
+ 
+				int shaker = 10;
+                //生成新窗口坐标
+                if(flag)
+                {
+                    posX=rect.left+rand()%shaker*5;
+                    posY=rect.top+rand()%shaker*5;
+                }               
+                else
+                {
+                    posX=rect.left-rand()%shaker*5;
+                    posY=rect.top-rand()%shaker*5;
+                }
+                 
+                //边境判断
+                if(posX<0)    posX=0;
+                if(posY<0)    posY=0;
+                if(posX+cX>sysX)
+                    posX=sysX-cX;
+                if(posY+cY>sysY)
+                    posY=sysY-cY;
+ 
+                //设置窗口位置
+                SetWindowPos(hWnd,HWND_NOTOPMOST,posX,posY,rect.right-rect.left,
+                    rect.bottom-rect.top,SWP_SHOWWINDOW | SWP_NOSIZE | SWP_NOACTIVATE );
+            }
+        }
+		Itime t2=GetCurrentITime();
+		if((t2-t1).ItimeToSecond()>5)
+		{
+			break;
+		}
+    }
+}
